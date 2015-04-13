5bb19c9c35e5c5247c1b7424e1f3ac2da02db23b
robin <863579016@qq.com>
2015-04-13 13:00:29 +0800

    chaper 02

---
+++ chapter02/checkword.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Filename:checkword.sh
+word=$1
+grep "^$1$" /usr/share/dict/american-english -q
+if [ $? -eq 0 ];
+then
+  echo $word is a dictionary word;
+else
+  echo $word is not a dictionary word;
+fi
+
---
+++ chapter02/interactive.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Filename: interactive.sh
+
+read -p "Enter number:" no;
+read -p "Enter name:" name;
+echo You have entered $no , $name;

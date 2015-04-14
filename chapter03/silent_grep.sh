c4363bfe0853e0587db4f1157d16ce071b6740da
robin <863579016@qq.com>
2015-04-14 23:45:17 +0800

    chapter01

---
+++ chapter03/silent_grep.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Filename: silent_grep.sh
+#Description: Testing whether a file contain a text or not
+if [ $# -ne 2 ];
+then 
+echo "$0 match_text filename"
+fi
+match_text=$1
+filename=$2
+
+grep -q $match_text $filename
+if [ $? -eq 0 ];
+then
+echo "The text exists in the file"
+else
+echo "Text does not exist in the file"
+fi
+
